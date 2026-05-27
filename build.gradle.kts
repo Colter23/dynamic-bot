c6b892cce96b0e24054e75e29b6fa8b4aac33908
Colter23 <colter_null@163.com>
2026-05-27 20:20:35 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
-    kotlin("plugin.serialization") version "2.0.20" // 临时
+    kotlin("jvm") version "2.3.21"
+    kotlin("plugin.serialization") version "2.3.21" // 临时
 }
 
 group = "top.colter.dynamic"
